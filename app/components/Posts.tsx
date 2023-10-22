bd78754cd687101e32a716773930e47259996dc1
Kristoffer <kristoffer.aber12@gmail.com>
2023-10-22 14:30:25 +0200

    use global IPost and remove fetch

--- app/components/Posts.tsx
+++ app/components/Posts.tsx
@@ -1,25 +1,14 @@
-type post = {
-    id: number,
-    title: string,
-    body: string,
-    userId: string,
+interface props {
+    posts: IPost[]
 }
 
 export const Posts = ({ posts }: props) => {
-    const response = await fetch('http://localhost:3000/api/posts');
-
-    return await response.json();
-}
-
-const Posts = async () => {
-    const posts = await fetchPosts();
-
     return (
         <div className='posts'>
-            {posts.map((post: post) => (
+            {posts.map((post: IPost) => (
                 <div key={post.id} className='card'>
-                    <h2>{post?.title}</h2>
-                    <p>{post?.body}</p>
+                    <h2>{post.title}</h2>
+                    <p>{post.body}</p>
                 </div>
             ))}
         </div>
