4e73dedc5601fd90510e9da6821cc7f721d199da
Kristoffer <kristoffer.aber12@gmail.com>
2023-10-22 14:29:42 +0200

    pass props to Posts component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 'use client'
 import { useState, useEffect } from 'react'
+import { Posts } from './components/Posts'
 
 export default function Home() {
+  const [posts, setPosts] = useState<IPost[]>([])
   return (
     <div>
       <h1>Welcome to KTR Web development</h1>
       <Posts />
+      <Posts posts={posts} />
     </div>
   )
 }
