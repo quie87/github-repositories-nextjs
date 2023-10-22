1924a202027e816344fdfea9eb861b01c94dbbf9
Kristoffer <kristoffer.aber12@gmail.com>
2023-10-22 14:27:30 +0200

    Adds Search component for Posts

---
+++ app/components/PostsSearch.tsx
@@ -0,0 +1,28 @@
+'use client'
+
+import { useState } from 'react'
+
+type props = {
+    getSearchResults: (posts: IPost[]) => void
+}
+
+export const PostsSearch = ({ getSearchResults }: props) => {
+    const [query, setQuery] = useState('')
+
+    const handleSubmit = async (event: React.FormEvent) => {
+        event.preventDefault()
+
+        const res = await fetch(`/api/posts/search?query=${query}`)
+        const posts: IPost[] = await res.json()
+        getSearchResults(posts)
+    }
+
+    return (
+        <form onSubmit={handleSubmit} className='search-form'>
+            <input type="text" className='search-input' placeholder='Search posts' value={query} onChange={(e) => setQuery(e.target.value)} />
+            <button className='search-button' type='submit'>
+                Search
+            </button>
+        </form>
+    )
+}
