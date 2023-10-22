a89a59b8039b62722298485a9e5924c060a61911
Kristoffer <kristoffer.aber12@gmail.com>
2023-10-22 14:31:01 +0200

    Adds search component to Home with state for loading and fetch

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,32 @@
 'use client'
 import { useState, useEffect } from 'react'
 import { Posts } from './components/Posts'
+import LoadingPage from './loading'
+import { PostsSearch } from './components/PostsSearch'
 
 export default function Home() {
   const [posts, setPosts] = useState<IPost[]>([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await fetch('/api/posts')
+      const data = await res.json()
+      setPosts(data)
+      setLoading(false)
+    }
+
+    fetchPosts()
+  }, [])
+
+  if (loading) {
+    return <LoadingPage />
+  }
+
   return (
     <div>
       <h1>Welcome to KTR Web development</h1>
-      <Posts />
+      <PostsSearch getSearchResults={(results: IPost[]): void => setPosts(results)} />
       <Posts posts={posts} />
     </div>
   )
